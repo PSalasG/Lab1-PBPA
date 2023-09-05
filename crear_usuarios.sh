d3a9863b5e2c05b754665ec176e3559c12f421f0
Pablo <pablo.salas@ucr.ac.cr>
2023-09-05 17:40:13 -0600

    Cambios realizados al script de crear_usuarios.sh

--- crear_usuarios.sh
+++ crear_usuarios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo useradd -m -N $1
+sudo useradd -m -N $1 # Creo el ususario sin un grupo propio para no crear grupos innecesarios.
 
 if [ $? = 9 ]; then
 	echo "El usuario $1 ya existe"
@@ -9,7 +9,7 @@ else
 	echo "El usuario $1 fue creado con éxito"
 fi
 
-sudo groupadd $2
+sudo groupadd $2 # Crea el grupo e imprime la confirmación.
 
 if [ $? = 9 ]; then
         echo "El grupo $2 ya existe"
@@ -18,12 +18,13 @@ else
 	echo "El grupo $2 fue creado con éxito"
 fi
 
-
+# Añade los usuarios al nuevo grupo.
 sudo usermod -a -G $2 $1
 sudo usermod -a -G $2 $USER
 
 echo "Los usuarios $1 y $USER fueron añadidos al grupo $2"
 
+#Cambia los permisos del anterior script.
 sudo chown :$2 ver_permisos.sh
 
 echo "El archivo ver_permisos.sh solo puede ser ejecutado por miembros del grupo $2"
